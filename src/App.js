bf2516c99fcb6f85b2e9b02ee858096cf447c0d2
vikipretium <vighneshpasupathy@gmail.com>
2022-07-24 16:57:37 +0530

    configure api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import PropTypes from 'prop-types';
+import Greeting from './Greeting';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store/configureStore';
+class App extends React.Component {
+  render() {
+    return (
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Greeting />} />
+            </Routes>
+          </Router>
+        </Provider>
+      </React.StrictMode>
+    );
+  }
 }
-
 export default App;
